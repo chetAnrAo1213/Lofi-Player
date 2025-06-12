d1d00304560d752fd1c5cb36c1c6fafbe1100bc1
Chetan Rao <167339150+chetAnrAo1213@users.noreply.github.com>
2025-06-12 22:06:17 +0530

    intial commit

--- src/Tracks/Mylesxiety.jsx
+++ src/Tracks/Mylesxiety.jsx
@@ -6,7 +6,7 @@ function Mylesxiety()
         <>
              <div style={{ position: "relative", width: "100%", height: "100%" }}>
       <video
-        src="https://raw.githubusercontent.com/chetAnrAo7/LofiPlayer-Data/main/Beach%20Drives.mp4"
+        src=""
         autoPlay loop playsInline muted
         style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", objectFit: "contain",
           zIndex: -1, backgroundColor: "black"}} />
