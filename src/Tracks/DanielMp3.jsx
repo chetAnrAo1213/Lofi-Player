a01e86a26f5e2e121e1a3458186f5140f62851e9
Chetan Rao <167339150+chetAnrAo1213@users.noreply.github.com>
2025-06-12 22:05:20 +0530

    Update DanielMp3.jsx

--- src/Tracks/DanielMp3.jsx
+++ src/Tracks/DanielMp3.jsx
@@ -6,7 +6,7 @@ function DanielMp3() {
     
     <div style={{ position: "relative", width: "100%", height: "100%" }}>
       <video
-        src="https://raw.githubusercontent.com/chetAnrAo7/LofiPlayer-Data/main/CoverPageVideo/It%20Will%20Be%20Alright.mp4"
+        src=""
         autoPlay loop playsInline muted
         style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", objectFit: "contain",
           zIndex: -1, backgroundColor: "black"}} />
