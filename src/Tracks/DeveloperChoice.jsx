819804d78b15e9abeb1775415a5102396c53b8bd
Chetan Rao <167339150+chetAnrAo1213@users.noreply.github.com>
2025-06-12 22:05:34 +0530

    intial commit

--- src/Tracks/DeveloperChoice.jsx
+++ src/Tracks/DeveloperChoice.jsx
@@ -6,7 +6,7 @@ function DeveloperChoice()
         <>
         <div style={{ position: "relative", width: "100%", height: "100%" }}>
       <video
-        src="https://raw.githubusercontent.com/chetAnrAo7/LofiPlayer-Data/main/CoverPageVideo/Developer%20Choice.mp4"
+        src=""
         autoPlay loop playsInline muted
         style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%", objectFit: "contain",
           zIndex: -1, backgroundColor: "black"}} />
